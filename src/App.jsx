86eb81fd804676019bab93123d816e3ade3f7075
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-10-14 08:20:01 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <Navbar />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-1 bg-[#36353d]"></main>
+        <footer className="bg-gray-800 p-5"></footer>
+      </div>
     </>
   );
 }
