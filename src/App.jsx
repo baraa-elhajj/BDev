3f4ab0b3d3b6cb961078fa90e463bf718ca85fdf
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-10-15 04:00:49 +0300

    Update App.jsx
    changed about to home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BriefcaseBusiness } from "lucide-react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
@@ -8,32 +9,58 @@ function App() {
         <Navbar />
         <main className="flex-1 bg-[#36353d]">
           <section
-            id="about"
-            className="flex flex-col md:flex-row items-center justify-between gap-10 px-6 lg:px-16 py-16"
+            id="home"
+            className="flex flex-col-reverse md:flex-row items-center justify-between gap-10 px-6 
+            lg:px-16 py-16"
           >
             <div className="md:w-1/2 space-y-6 text-center md:text-left">
+              <div className="block md:hidden mb-4">
+                <img
+                  src="src/assets/images/profile.png"
+                  alt="Profile"
+                  className="w-30 h-30 rounded-full object-cover mx-auto"
+                />
+              </div>
               <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-[#ffbd59] leading-tight">
-                Hello, I'm Baraa!
+                Code with purpose. Build with passion.
               </h2>
 
               <p className="text-base sm:text-lg text-white/85 leading-relaxed">
-                I'm a Full-stack Engineer with 4+ years of experience building
-                and optimizing web applications. I love working with modern
-                tools like <span className="text-[#ffbd59]/85">React</span>,{" "}
+                I build web apps with modern tools like{" "}
+                <span className="text-[#ffbd59]/90">React</span>,{" "}
                 <span className="text-[#ffbd59]/85">Next.js</span>, and{" "}
-                <span className="text-[#ffbd59]/85">MERN</span> stack, and I
-                have hands-on experience creating and deploying REST APIs,
-                integrating databases, and delivering scalable solutions.
-                Passionate about continuous learning and currently expanding my
-                expertise through personal projects and certifications.
+                <span className="text-[#ffbd59]/85">MERN</span> stack.<br></br>{" "}
+                My passion for building and solving problems through code
+                thrives me to push boundaries and turn ideas into reality.
               </p>
+
+              <div className="flex items-center justify-center sm:justify-start space-x-2 lg:space-x-3">
+                <a
+                  href="#projects"
+                  className="lg:space-x-2 px-3 lg:px-4 py-1.5 lg:py-2 text-xs lg:text-sm font-semibold 
+                  text-white/85 dark:text-white/80 border border-[#ffbd59] dark:border-gray-700 
+                  rounded-md hover:bg-[#ffbd59] hover:text-[#36353d]/95 dark:hover:bg-gray-800 
+                  transition-colors duration-300"
+                >
+                  Some of my projects
+                </a>
+                <button
+                  className="flex items-center justify-center cursor-pointer px-4 lg:px-6 py-1.5 lg:py-2 
+                  text-xs lg:text-sm font-semibold bg-[#ffbd59] text-[#36353d]/95 dark:bg-gray-50 
+                  dark:text-gray-900 rounded-md hover:bg-[#ffbd59]/80 dark:hover:bg-gray-200 
+                  transition-colors duration-300"
+                >
+                  <a href="#hire-me">Hire Me</a>
+                  <BriefcaseBusiness className="h-3 w-3 ml-1.5 mb-0.5 lg:h-4 lg:w-4" />
+                </button>
+              </div>
             </div>
 
-            <div className="md:w-1/2 flex justify-center">
+            <div className="hidden md:flex md:w-1/2 justify-center">
               <img
                 src="src/assets/images/profile.png"
                 alt="Profile"
-                className="w-100 h-100 hover:scale-105 transition-transform duration-300"
+                className="w-100 h-100"
               />
             </div>
           </section>
