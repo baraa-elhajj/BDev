f570240c52baef877410c320f84abb97de302cef
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-10-20 14:01:06 +0300

    Update Home.jsx
    added fade-in animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
+import { motion } from "framer-motion";
 import { BriefcaseBusiness } from "lucide-react";
-import Orbit from "./ui/Orbit";
 
 export default function Home() {
   return (
@@ -13,20 +13,39 @@ export default function Home() {
             className="w-40 h-50 rounded-full object-cover mx-auto "
           />
         </div>
-        <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-[#ffbd59] leading-tight">
-          Code with purpose. Build with passion.
-        </h2>
 
-        <p className="text-base sm:text-lg text-white/85 leading-relaxed">
+        <motion.h2
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          className="text-3xl sm:text-4xl lg:text-5xl font-bold text-[#ffbd59] leading-tight"
+        >
+          Code with purpose. Build with passion.
+        </motion.h2>
+
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ delay: 0.2, duration: 0.7 }}
+          className="text-base sm:text-lg text-white/85 leading-relaxed"
+        >
           I build web apps with modern tools like{" "}
           <span className="text-[#ffbd59]/90">React</span>,{" "}
           <span className="text-[#ffbd59]/85">Next.js</span>, and{" "}
           <span className="text-[#ffbd59]/85">MERN</span> stack.<br></br>
           My passion for building and solving problems through code thrives me
           to push boundaries and turn ideas into reality.
-        </p>
+        </motion.p>
 
-        <div className="flex space-x-3 items-center justify-center md:justify-start">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ delay: 0.3, duration: 0.7 }}
+          className="flex space-x-3 items-center justify-center md:justify-start"
+        >
           <a
             href="#projects"
             className="px-3 lg:px-4 py-1.5 lg:py-2 text-xs lg:text-sm font-semibold text-white/85 
@@ -43,18 +62,23 @@ export default function Home() {
             <a href="#hire-me">Hire Me</a>
             <BriefcaseBusiness className="h-3 lg:h-4 w-3 lg:w-4 ml-1.5 mb-0.5" />
           </button>
-        </div>
+        </motion.div>
       </div>
 
       {/* Desktop  */}
-      <div className="hidden md:flex w-1/2 justify-center">
-        {/* <Orbit /> */}
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.7 }}
+        className="hidden md:flex w-1/2 justify-center"
+      >
         <img
           src="src/assets/images/profile.png"
           alt="Profile"
           className="w-100 h-100"
         />
-      </div>
+      </motion.div>
     </section>
   );
 }
