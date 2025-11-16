29121d36cee3e18f638d8b842e4b168c2b4ba86f
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-16 20:17:27 +0200

    Create ThemeContext.jsx

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+
+  useEffect(() => {
+    const html = document.documentElement;
+
+    if (theme === "dark") {
+      html.classList.add("dark");
+      localStorage.theme = "dark";
+    } else {
+      html.classList.remove("dark");
+      localStorage.theme = "light";
+    }
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
