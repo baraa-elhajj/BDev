fcd388e6b316d136f242a31207aae076a38a9202
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-17 22:42:41 +0200

    Update ThemeToggle.jsx
    Add toggle animation

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import { useTheme } from "../context/ThemeContext";
 import { FiSun, FiMoon } from "react-icons/fi";
 
@@ -7,10 +8,32 @@ const ThemeToggle = () => {
   return (
     <button
       onClick={toggleTheme}
-      className="cursor-pointer p-2 rounded-lg bg-transparent text-(--navbar-text-color) 
+      className="cursor-pointer p-2 rounded-lg bg-transparent text-(--font-color-reverse-paragraph)
       hover:scale-110 transition-transform duration-200"
     >
-      {theme === "dark" ? <FiSun size={18} /> : <FiMoon size={18} />}
+      <AnimatePresence mode="wait">
+        {theme === "dark" ? (
+          <motion.div
+            key="sun"
+            initial={{ y: -5, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 5, opacity: 0 }}
+            transition={{ duration: 0.15 }}
+          >
+            <FiSun size={18} />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="moon"
+            initial={{ y: -5, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 5, opacity: 0 }}
+            transition={{ duration: 0.15 }}
+          >
+            <FiMoon size={18} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </button>
   );
 };
