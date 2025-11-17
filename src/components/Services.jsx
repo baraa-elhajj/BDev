6e91f31a4d2961e5d3bea0d9212714f63799dcb1
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-17 21:46:06 +0200

    Apply theme to Services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -58,7 +58,7 @@ export default function Services() {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         transition={{ duration: 0.7 }}
-        className="text-3xl sm:text-4xl font-bold text-[#ffbd59] mb-5"
+        className="text-3xl sm:text-4xl font-bold text-(--font-color-header) mb-5"
       >
         My Services
       </motion.h2>
@@ -68,7 +68,7 @@ export default function Services() {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         transition={{ delay: 0.1, duration: 0.7 }}
-        className="space-y-4 text-white/85 text-md sm:text-lg leading-relaxed max-w-3xl mb-8"
+        className="space-y-4 text-(--font-color-paragraph)/85 text-md sm:text-lg leading-relaxed max-w-3xl mb-8"
       >
         <p>
           I provide a range of development services leveraging modern
@@ -86,8 +86,8 @@ export default function Services() {
         {services.map((service, index) => (
           <div
             key={index}
-            className="relative bg-transparent border border-[#ffbd59] text-white/85 p-6 max-w-md rounded-lg shadow-lg 
-            hover:scale-101 transition-transform duration-300"
+            className="relative bg-transparent border border-(--font-color-header) text-(--font-color-paragraph)/85 
+            p-6 max-w-md rounded-lg shadow-lg hover:scale-101 transition-transform duration-300"
           >
             <div className="flex items-center mb-4">
               <div className="mr-4">{service.icon}</div>
@@ -95,14 +95,14 @@ export default function Services() {
                 {service.title}
               </h3>
             </div>
-            <p className="mb-4 text-xs sm:text-base text-white/80">
+            <p className="mb-4 text-xs sm:text-base text-(--font-color-paragraph)/80">
               {service.description}
             </p>
             <div className="flex flex-wrap gap-2">
               {service.badges.map((badge, idx) => (
                 <span
                   key={idx}
-                  className="bg-transparent border border-[#ffbd59] text-[#ffbd59] 
+                  className="bg-transparent border border-(--font-color-header) text-(--font-color-header) 
                   px-2 py-0.5 sm:px-2.5 sm:py-1 text-xs sm:text-sm
                   rounded-lg font-medium"
                 >
