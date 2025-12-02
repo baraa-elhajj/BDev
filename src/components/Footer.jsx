fabf9510f1fe3ec7d2124083414331e890c19c8f
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-12-02 13:25:50 +0200

    Update Footer.jsx
    update name in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-[#111827] text-gray-300 py-8 px-6">
       <p className="mx-auto text-center text-white/80 text-xs sm:text-sm font-medium">
-        BDev © {new Date().getFullYear()} All rights reserved.
+        Baraa El Hajj © {new Date().getFullYear()} All rights reserved.
       </p>
     </footer>
   );
